18baa6bc845f5401591a25996783173e53a8f6b7
Vladislav Demianenko <101254541+OlegMamedov@users.noreply.github.com>
2025-04-06 14:30:14 +0300

    fix dockerfile

--- init_db.py
+++ init_db.py
@@ -8,12 +8,16 @@ load_dotenv()
 
 # DATABASE
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "88355247")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "hotel_test")
 
 POSTGRES_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+print(
+    f"Connecting to database with URL: {POSTGRES_URL}"
+)  # Добавляем логирование для отладки
 
 
 async def init_db():
